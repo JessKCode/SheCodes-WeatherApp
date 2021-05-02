7a858924d9d341c45ac0430dd38f33a43f18acd9
JessKCode <82847253+JessKCode@users.noreply.github.com>
2021-05-02 15:01:55 -0700

    Streamlined/cleaned up js

--- app.js
+++ app.js
@@ -22,80 +22,12 @@ if (minute > 10) {
 let dayTime = document.querySelector("h3");
 dayTime.innerHTML = `${day} ${hours}:${minute}`;
 
-//City Search and change
-function cityChange(event) {
-  event.preventDefault();
-  let input = document.querySelector("#search-input");
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `${input.value}`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=e411a3752881f98038e4e57881b9b78f&units=imperial`;
-  
-  axios.get(apiUrl).then(showWeather);
-  console.log(apiUrl);
-}
-  function showWeather(response) {
-    let dayTemp = Math.round(response.data.main.temp);
-    let headTemp = `${dayTemp}°`;
-    let h2 = document.querySelector("h2");
-    h2.innerHTML = headTemp;
-
-    let maxTemp = Math.round(response.data.main.temp_max);
-    let minTemp = Math.round(response.data.main.temp_min);
-    let highLow = ` H:${maxTemp}° L:${minTemp}°`;
-    let hl = document.querySelector(".topHighLow");
-    hl.innerHTML = highLow;
-    
-    let description =(response.data.weather[0].description);
-    let weatherDescription = document.querySelector(".weather-description");
-    weatherDescription.innerHTML = `${description}`;
-
-    let windSpeed = Math.round(response.data.wind.speed);
-    let speed = document.querySelector(".wind");
-    speed.innerHTML = `Wind Speed: ${windSpeed} mph`;
-    
-    let humidity =(response.data.main.humidity);
-    let humid = document.querySelector(".humidity");
-    humid.innerHTML = `Humidity: ${humidity}%`;
-
-    //icon changer
-
-    let weather= (response.data.weather[0].icon);
-
-  let icon =document.querySelector(".topIcon");
-if (weather === `01d`){
-    icon.innerHTML =(`<i class="fas fa-sun"></i>`);}
-if (weather === `01n`){
-    icon.innerHTML =(`<i class="fas fa-moon"></i>`);}
-if (weather === `02d`||weather === "04d"){
-    icon.innerHTML =(`<i class="fas fa-cloud-sun"></i>`);}
-if (weather === "02n"||weather === "04n"){
-    icon.innerHTML =(`<i class="fas fa-cloud-moon"></i>`);}
-if (weather === "03d" || weather === "03n"){
-    icon.innerHTML =(`<i class="fas fa-cloud"></i>`);}  
-if (weather === "09d"|| weather === "09n"){
-    icon.innerHTML =(`<i class="fas fa-cloud-rain"></i>`);}
-if (weather === "10d"|| weather === "10n"){
-   icon.innerHTML =(`<i class="fas fa-cloud-showers-heavy"></i>`);}
-if (weather === "11d"|| weather === "11n"){
-   icon.innerHTML =(`<i class="fas fa-bolt"></i>`);}
-if (weather === "13d"|| weather === "13n"){
-    icon.innerHTML =(`<i class="far fa-snowflake"></i>`);}
-if (weather === "50d"|| weather === "50n"){
-    icon.innerHTML =(`<i class="fas fa-smog"></i>`);}
-} 
-
-
-let city = document.querySelector("#search-city");
-city.addEventListener("submit", cityChange);
-
-//Search by location
-function showCurrentWeather(response) {
+//Display weather in top box
+function showWeather(response) {
   let temperature = Math.round(response.data.main.temp);
-  let headTemp = `${temperature}°F`;
+  let headTemp = `${temperature}°`;
   let h2 = document.querySelector("h2");
   h2.innerHTML = headTemp;
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `My Location`;
 
   let description =(response.data.weather[0].description);
     let weatherDescription = document.querySelector(".weather-description");
@@ -136,12 +68,30 @@ if (weather === "50d"|| weather === "50n"){
     icon.innerHTML =(`<i class="fas fa-smog"></i>`);}
 }
 
+//Search by City
+function cityChange(event) {
+  event.preventDefault();
+  let input = document.querySelector("#search-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${input.value}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=e411a3752881f98038e4e57881b9b78f&units=imperial`;
+  
+  axios.get(apiUrl).then(showWeather);
+  console.log(apiUrl);
+}
+  
+let city = document.querySelector("#search-city");
+city.addEventListener("submit", cityChange);
+
+//Search by location
 function displayPosition(position) {
   let lat = position.coords.latitude;
   let long = position.coords.longitude;
   let apiKey = `e411a3752881f98038e4e57881b9b78f`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`;
-  axios.get(apiUrl).then(showCurrentWeather);
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `My Location`;
+  axios.get(apiUrl).then(showWeather);
 }
 
 function getLocation(place) {
